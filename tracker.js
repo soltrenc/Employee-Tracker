b2e22682641ade0e97520626e4782e9d5ddbe45c
Chris Soltren <csoltren1@yahoo.com>
2020-03-17 21:20:07 -0400

    Added .env file

--- tracker.js
+++ tracker.js
@@ -1,64 +1,65 @@
 const mysql = require("mysql");
-const inquirer = require("inquirer");
+const { prompt } = require("inquirer");
 const figlet = require("figlet");
 const conTable = require("console.table")
-
+const util = require("util");
+require('dotenv').config();
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "iNotlNN4",
+    password: process.env.DB_pass,
     database: "tracker_DB"
 });
 
 init();
-
-async function init() {
-    connection.connect();
-    //displayFiglet();
-    await promptUser();
-    console.log('hello')
-}
-
-async function displayFiglet() {
-    await figlet('Employee Tracker', function (err, text) {
+function init() {
+    figlet('Employee Tracker', function (err, text) {
         if (err) {
             console.log('something went wrong...');
             console.dir(err);
             return;
         }
         console.log(text);
+        promptUser();
     });
+
 }
 async function promptUser() {
-    inquirer
-        .prompt([
-            {
-                name: "action",
-                type: "list",
-                message: "What would you like to do?",
-                choices: ["View all employees"]
-            }
-        ])
-        .then(answers => {
-            console.log(answers.action);
-            //viewAllEmployees();
-        })
-        .catch(error => {
-            if (error.isTtyError) { }
-            else { }
-        });
+    const { action } = await prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_ALL_EMPLOYEES"
+                },
+            ]
+        }
+    ])
+    switch (action) {
+        case "VIEW_EMPLOYEES":
+            return viewAllEmployeesAsync();
+        default:
+            return quit();
+    }
 }
 
-function viewAllEmployees() {
-    connection.query("SELECT * FROM tracker_DB.employee", function (err, results) {
-        if (err) throw err;
-        console.log(results)
-    })
+async function viewAllEmployeesAsync() {
+    connection.query = util.promisify(connection.query);
+    const employees = await connection.query("SELECT * FROM tracker_DB.employee");
+    console.log('employees', employees);
+    promptUser();
 }
 
 function addEmployee() {
     connection.query("SELECT * FROM tracker_DB.employee"), function (err, results) {
         add();
-    }
+    };
+    promptUser();
+}
+async function quit() {
+    return;
 }
